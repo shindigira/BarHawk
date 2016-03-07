0fcfaab52dc7b6b6cbab42f62ea3743a52032c7d
Michael Serna <michael.serna01@gmail.com>
2016-03-07 10:37:46 -0800

    (feat/server) Update changes to server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
 var express = require('express');
 
 var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello world!');
+});
+
+app.listen(port, function () {
+  console.log('Server now listening on port ' + port);
+});
