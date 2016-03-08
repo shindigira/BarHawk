81bb365f7e529986a83987a9424701ea7bfa42c1
Michael Serna <michael.serna01@gmail.com>
2016-03-07 20:07:06 -0800

    (feat) Add dummy data and error handling

--- client/app/auth/patron/patronAuth.js
+++ client/app/auth/patron/patronAuth.js
@@ -1,17 +1,37 @@
-angular.module('customerAuth', [])
+angular.module('asyncdrink.customerAuth', [])
 
-.controller('customerController', function($scope, customerFactory){
-  $scope.customerFactory
+.controller('customerController', function($scope, customerFactory) {
+    $scope.newUser = {};
 
-})
-.factory('customerFactory', function(){
-  var userInfo = {};
-  
-  userInfo.username = username;
-  userInfo.password = password;
-  userInfo.age = age;
-  userInfo.weight = weight;
-  userInfo.gender = gender;
+    $scope.signUp = function() {
+      console.log($scope.newUser)
+      customerFactory.signUp($scope.newUser);
+    };
+  })
+  .factory('customerFactory', function($http) {
+    //var userInfo = {};
 
-  return userInfo;
-});
+    // userInfo.username;
+    // userInfo.password;
+    // userInfo.age;
+    // userInfo.weight;
+    // userInfo.gender;
+
+    var signUp = function(userInfo) {
+      console.log('hit signup')
+      console.log("userInfo", userInfo)
+      return $http({
+          method: "POST",
+          url: '/api/users/signup',
+          data: JSON.stringify(userInfo)
+        }).then(function(data) {
+          res.send(data)
+        })
+        .catch(function(err) {
+          console.log(err);
+        })
+    };
+    return {
+      signUp: signUp
+    };
+  });
