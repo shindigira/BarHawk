198d7cc06f7043aeec93ff569978626bbc3a055e
Michael Serna <michael.serna01@gmail.com>
2016-03-08 15:26:16 -0800

    (feat) Implement order functionality

--- client/app/customer/options.js
+++ client/app/customer/options.js
@@ -1,11 +1,40 @@
 angular.module('asyncdrink.options', [])
 
 .controller('optionsController', function($scope, customerFactory, optionsFactory) {
+  $scope.currentUser = optionsFactory.currentUser;
+  $scope.order = {};
+  $scope.order.username = optionsFactory.currentUser;
+  $scope.order.time = new Date();
+  $scope.order.currentPrice = 5;
+  $scope.orderSuccess = false;
+  $scope.orderFail = false;
 
+
+  $scope.orderOnly = function() {
+    $scope.order.closeout = false;
+    optionsFactory.orderOnly($scope.order)
+      .then(function(response) {
+        $scope.orderSuccess = true;
+      }).catch(function(err) {
+        $scope.orderFail = true;
+      });
+
+  };
 })
 
 .factory('optionsFactory', function($http) {
-  return {
+  var currentUser;
 
-  }
+  var orderOnly = function(order) {
+    return $http({
+      method: "POST",
+      url: '/api/customer/order',
+      data: order
+    });
+  };
+
+  return {
+    currentUser: currentUser,
+    orderOnly: orderOnly,
+  };
 });
