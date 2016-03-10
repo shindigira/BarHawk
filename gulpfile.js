f8b09eacccaa9c78482127ddb58d4a1eda6e656b
Dan Novograd <dnovograd@gmail.com>
2016-03-10 12:14:51 -0800

    (dev) Complete gulp file and update index

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 //include gulp
 var gulp = require('gulp');
+//server for testing
+var Server = require('karma').Server;
 
 //plugins
 var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var cssmin = require('gulp-cssmin');
-
+var minify = require('gulp-minify');
+var nodemons = require('gulp-nodemon');
 //Lint Task
 gulp.task('lint', function() {
   return gulp.src('client/app/*.js')
@@ -16,22 +18,35 @@ gulp.task('lint', function() {
   .pipe(jshint.reporter('default'));
 });
 
-//Sass
+//CSS minify
 gulp.task('minify-css', function() {
   return gulp.src('client/styles/*.css')
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('client/dist'));
 });
 
 //Concatenate and Minify JS
 gulp.task('scripts', function() {
   return gulp.src('client/app/**/*.js')
   .pipe(concat('all.js'))
-  .pipe(gulp.dest('dist'))
-  .pipe(rename('all.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('client/dist'));
+});
+
+//run nodemon
+gulp.task('server', function() {
+  nodemons({
+    script: 'server/server.js',
+    ext: 'js html',
+  })
+});
+
+//testing
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
 });
 
 //Watch files
@@ -40,4 +55,4 @@ gulp.task('watch', function() {
 });
 
 //default
-gulp.task('default', ['lint', 'minify-css', 'scripts', 'watch']);
+gulp.task('default', ['lint', 'minify-css', 'scripts', 'test', 'server', 'watch']);
