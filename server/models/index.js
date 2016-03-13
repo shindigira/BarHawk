6e9b0fbb511a7d6b018fabf54b79b28ebad60596
Dan Novograd <dnovograd@gmail.com>
2016-03-12 17:52:02 -0800

    (feat) Establish connection to deployed database

--- server/models/index.js
+++ server/models/index.js
@@ -9,14 +9,7 @@ var config    = require(__dirname + '/../config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable],
-    {
-      dialect: 'postgres',
-      protocol: 'postgres',
-      port: match[4],
-      host: match[3],
-      logging: true
-    });
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
