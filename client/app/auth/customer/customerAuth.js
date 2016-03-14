7292ff07393434bebf165d5d094519508d08443d
Michael Serna <michael.serna01@gmail.com>
2016-03-14 11:25:22 -0700

    (refactor) Make changes to form validation

--- client/app/auth/customer/customerAuth.js
+++ client/app/auth/customer/customerAuth.js
@@ -7,37 +7,20 @@ angular.module('asyncdrink.customerAuth', [])
     drinkCount: 0,
     totalPrice: 0
   };
-  //dummy data for easy submit
-  // $scope.newUser.username= 'Beyonce';
-  // $scope.newUser.password = "pass";
-  // $scope.newUser.age = 27;
-  // $scope.newUser.weight = 160;
+
 
   $scope.loginAttempt = {};
 
   $scope.invalidLogIn = false;
 
 
-  $scope.clear = function() {
+  $scope.clear = function () {
     $scope.invalidLogIn = false;
   };
 
-  // $scope.validateSignIn = function(){
-  //   if(!$scope.loginAttempt.username){
-  //     alert('you forgot to enter your username');
-  //     return;
-  //   }
-  //   if(!$scope.loginAttempt.password){
-  //     alert('you forgot to enter your password');
-  //     return;
-  //   }
-  //   console.log('validateSignIn passed both if statements');
-  //   $scope.logIn();
-
-  // }
 
 
-   $scope.submitForm = function(isValid) {
+  $scope.submitForm = function (isValid) {
 
     // check to make sure the form is completely valid
     if (isValid) {
@@ -47,38 +30,10 @@ angular.module('asyncdrink.customerAuth', [])
   };
 
 
-  // $scope.validateSignUp = function() {
-  //   console.log($scope.newUser);
-
-  //   if (!$scope.newUser.username) {
-  //     alert('you forgot to setup your username');
-  //     return;
-  //   }
-  //   if (!$scope.newUser.password) {
-  //     alert('you forgot to setup your password');
-  //     return;
-  //   }
-  //   if (!$scope.newUser.age) {
-  //     alert('you forgot to enter your age');
-  //     return;
-  //   }
-  //   if (!$scope.newUser.weight) {
-  //     alert('you forgot to enter your weight');
-  //     return;
-  //   }
-  //   if (!$scope.newUser.gender) {
-  //     alert('you forgot to select your gender');
-  //     return;
-  //   }
-  //   console.log('else happen')
-  //   $scope.signUp();
-
-  // };
-
-  $scope.signUp = function() {
+  $scope.signUp = function () {
 
     customerFactory.signUp($scope.newUser)
-      .then(function(response) {
+      .then(function (response) {
         //hide error message, if displayed
         $scope.invalidSignup = false;
 
@@ -89,16 +44,16 @@ angular.module('asyncdrink.customerAuth', [])
         //navigate to options page
         $state.go('options');
       })
-      .catch(function(error) {
+      .catch(function (error) {
         //show user that they failed to signup successfully
         $scope.invalidSignup = true;
       });
   };
 
-  $scope.logIn = function() {
+  $scope.logIn = function () {
 
     customerFactory.signIn($scope.loginAttempt)
-      .then(function(response) {
+      .then(function (response) {
         //hide error message, if displayed
         $scope.invalidLogIn = false;
         //persist logged in user
@@ -108,7 +63,7 @@ angular.module('asyncdrink.customerAuth', [])
         //navigate to options page
         $state.go('options');
       })
-      .catch(function(error) {
+      .catch(function (error) {
         //display invalid login message
         $scope.invalidLogIn = true;
       });
@@ -118,28 +73,28 @@ angular.module('asyncdrink.customerAuth', [])
 .factory('customerFactory', function ($http, $window) {
   var signIn = function (loginAttempt) {
     return $http({
-      method: "POST",
-      url: '/api/users/login',
-      data: loginAttempt
-    })
-    .then(function(resp){
-      return resp.data;
-    })
+        method: "POST",
+        url: '/api/users/login',
+        data: loginAttempt
+      })
+      .then(function (resp) {
+        return resp.data;
+      })
   };
 
-  var signUp = function(userInfo) {
+  var signUp = function (userInfo) {
     console.log('we are inside signUP HTTP');
     return $http({
-      method: "POST",
-      url: '/api/users/signup',
-      data: userInfo
-    })
-    .then(function(resp){
-      return resp.data;
-    });
+        method: "POST",
+        url: '/api/users/signup',
+        data: userInfo
+      })
+      .then(function (resp) {
+        return resp.data;
+      });
   };
 
-  var isAuth = function(){
+  var isAuth = function () {
     return !!$window.localStorage.getItem('com.barhawk');
 
     //the below is a way to check if token exists and is correct for that particular user
