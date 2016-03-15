4c925adb296375be855ad3fd54ca4641e0ef0191
Nadine Ott <nadineott7@yahoo.com>
2016-03-14 19:28:16 -0700

    (test) Add angular-mocks.js and basic tests for customer controller

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'client/app/library/angular-mocks.js',
       'specs/*.js'
     ],
 
--- specs/testing.js
+++ specs/testing.js
@@ -1,8 +1,53 @@
-describe("this testing suite", function() {
- it ("should show that true is true", function() {
-   expect(true).toEqual(true);
- })
- it ("should show that false is not true", function() {
-   expect(false).not.toBe(true);
- })
+describe("this testing suite", function () {
+  it("should show that true is true", function () {
+    expect(true).toEqual(true);
+  })
+  it("should show that false is not true", function () {
+    expect(false).not.toBe(true);
+  })
 });
+
+describe('customerController', function () {
+  var $scope;
+  var $state; 
+  var $window; 
+  var customerFactory;
+  var optionsFactory;
+
+  beforeEach(module('asyncdrink'));
+
+  
+  // beforeEach(inject(function ($injector) {
+  //   //mock out dependencies
+  //   $state = $injector.get('$state');
+  //   $window = $injector.get('$window');
+  //   customerFactory = $injector.get('customerFactory');
+  //   optionsFactory = $injector.get('optionsFactory');
+  //   $scope = $rootScope.$new();
+  //   var $controller = $injector.get('$controller');
+
+  //   createController = function () {
+  //     return $controller('customerController', {
+  //       $scope: $scope,
+  //       $state: $state,
+  //       $window: $window,
+  //       customerFactory: customerFactory,
+  //       optionsFactory: optionsFactory
+  //     });
+  //   };
+  //   createController();
+  // }));
+
+  // it('should have a newUser property on the $scope', function () {
+  //   expect($scope.newUser).to.be.an('object');
+  // });
+
+  // it('should have a loginAttempt property on the $scope', function () {
+  //   expect($scope.loginAttempt).to.be.an('object');
+  // });
+
+  // it('should have an invalidLogin property set to false', function () {
+  //   expect($scope.invalidLogin).to.be(false);
+  // });
+
+})
