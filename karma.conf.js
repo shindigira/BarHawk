523ef1d2129e6ac681a5452a239c4cde9a3847d0
Nadine Ott <nadineott7@yahoo.com>
2016-03-15 16:52:12 -0700

    (test) Remove white space + update npm

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,6 @@ module.exports = function (config) {
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {},
 
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
