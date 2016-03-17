aab21ceb828ce859635b6d9ec1b3d96f832ed14c
Dan Novograd <dnovograd@gmail.com>
2016-03-17 12:13:34 -0700

    (feat) Implement secure passwords

--- server/customers/customerController.js
+++ server/customers/customerController.js
@@ -1,64 +1,75 @@
 var jwt = require('jwt-simple');
 var models = require('../models');
 var db = require('../models/index.js');
+var bcrypt = require('bcrypt');
 
 module.exports = {
   login: function (req, res) {
     //set username/password request to attempt variable
     var attempt = req.body;
+    console.log("LOGIN ATTEMPT", attempt)
+
     models.users.findOne({
-      where: { username: attempt.username }
-    }).then(function (result) {
-      if (result === null) {
-        res.sendStatus(401)
-      } else {
-        if (attempt.password === result.dataValues.password) {
-          var token = jwt.encode(attempt.username, 'barHawksecret444');
-          res.json({
-            currentUser: result,
-            token: token
-          });
-        } else {
+        where: { username: attempt.username },
+      }).then(function (result) {
+          var hashedPassword = result.dataValues.password;
+          bcrypt.compare(attempt.password, hashedPassword, function(err, comparison) {
+            if (comparison) {
+              var token = jwt.encode(attempt.username, 'barHawksecret444');
+              res.json({
+                currentUser: result,
+                token: token
+              });
+            }
+            else {
+              res.sendStatus(401);
+            }
+          })
+        }).catch(function(err) {
           res.sendStatus(401);
-        }
-      }
-    });
+        })
   },
 
   signup: function (req, res) {
     //assigning drink order to variable
     var attempt = req.body;
+    var hashedPW;
+    //hash passwords
+    bcrypt.hash(attempt.password, 10, function (err, hash) {
+      hashedPW = hash;
 
-    //find existing user (err) or create new (success)
-    models.users.findOrCreate({
-      where: { username: attempt.username },
-      defaults: {
-        firstname: attempt.firstname,
-        lastname: attempt.lastname,
-        password: attempt.password,
-        age: attempt.age,
-        weight: attempt.weight,
-        gender: attempt.gender,
-        photo: attempt.photo,
-        phone: attempt.phonenumber
-      }
-    }).spread(function (user, created) {
-      console.log("able to create new user " + attempt.username + "?", created);
-      // //returns preexisting user
-      var userObj = user.get({
-        plain: false
-      });
-      if (created) {
-        var token = jwt.encode(user, 'barHawksecret444');
-        console.log(attempt);
-        res.json({
-          currentUser: attempt,
-          token: token
+      console.log("ATTEMPTED PASSWORD", hashedPW);
+      //find existing user (err) or create new (success)
+      models.users.findOrCreate({
+        where: { username: attempt.username },
+        defaults: {
+          firstname: attempt.firstname,
+          lastname: attempt.lastname,
+          password: hashedPW,
+          age: attempt.age,
+          weight: attempt.weight,
+          gender: attempt.gender,
+          photo: attempt.photo,
+          phone: attempt.phonenumber
+        }
+      }).spread(function (user, created) {
+        console.log("able to create new user " + attempt.username + "?", created);
+        // //returns preexisting user
+        var userObj = user.get({
+          plain: false
         });
-        //res.send(userObj);
-      } else {
-        res.sendStatus(401);
-      }
+        if (created) {
+          var token = jwt.encode(user, 'barHawksecret444');
+          console.log(attempt);
+          res.json({
+            currentUser: attempt,
+            token: token
+          });
+          //res.send(userObj);
+        } else {
+          res.sendStatus(401);
+        }
+      });
     });
   },
 
