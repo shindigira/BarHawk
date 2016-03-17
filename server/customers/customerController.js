f46dbcb60502f13fe4bf86b196eb8b446764acb2
Dan Novograd <dnovograd@gmail.com>
2016-03-16 17:49:50 -0700

    (feat) Draft BAC calculation

--- server/customers/customerController.js
+++ server/customers/customerController.js
@@ -77,6 +77,39 @@ module.exports = {
     .then(function(userdrinks) {
       res.send(userdrinks[0].dataValues.drinkCount)
     })
+  },
+
+  getBAC: function(req, res) {
+    var user = req.body.username;
+
+    //set current time
+    var time = new Date();
+
+    //insert this into query of camel-case column
+    var timeFulfilled = '"updatedAt"';
+    //limit drinks to 12 hours ago
+    time.setHours(time.getHours() - 12);
+    //convert to compare with updatedAt column
+    time = time.toISOString();
+    //query username, weight, gender, total alcohol consumed (g), and earliest drink within last 12 hours
+    db.sequelize.query("select orders.username, users.weight, users.gender, sum(drinks.alcohol) AS totalAlcohol, min(orders." + timeFulfilled + ") AS starttime from orders, users, drinks WHERE orders." + timeFulfilled + " > '" + time + "' AND orders.username = '" + user + "' AND orders.username = users.username AND orders.drinktype = drinks.name group by orders.username, users.weight, users.gender;")
+    .then(function (result) {
+      //result will be single-row table of arguments for BAC formula (username, weight, gender, totalalcohol, starttime)
+      var argumentsObj = result[0][0];
+      var elapsedTime = Math.floor((argumentsObj.starttime - time)/1000/60/60);
+      //gender constants
+      var rObject = {
+        male: .68,
+        female: .55
+      }
+      //convert weight to grams
+      var weightInGrams = function(weight) {
+        return (argumentsObj.weight * 453.592);
+      };
+
+      var BAC = (100 * ((argumentsObj.totalalcohol / (weightInGrams(argumentsObj.weight) * rObject[argumentObj.gender]) ) )) - (elapsedTime * .015);
+      res.send(BAC);
+    });
   }
 
 };
