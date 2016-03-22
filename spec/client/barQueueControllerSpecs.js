a42eb2bd9b08f383edd55328fabe3cce5f46689b
Collin Adams <collinadams04@gmail.com>
2016-03-21 19:15:16 -0700

    (refactor) Change front-end tests to match fixed poll code

--- spec/client/barQueueControllerSpecs.js
+++ spec/client/barQueueControllerSpecs.js
@@ -1,47 +1,47 @@
-describe('BarQueueController', function () {
+describe('BarQueueController', function() {
 
-  var $scope;
-  var $rootScope;
-  var $window;
-  var $state;
-  var OrdersFactory;
-  var optionsFactory;
+    var $scope;
+    var $rootScope;
+    var $window;
+    var $state;
+    var OrdersFactory;
+    var optionsFactory;
 
-  beforeEach(module('asyncdrink'));
+    beforeEach(module('asyncdrink'));
 
-  beforeEach(inject(function ($injector) {
+    beforeEach(inject(function($injector) {
 
-    $rootScope = $injector.get('$rootScope');
-    $state = $injector.get('$state');
-    $window = $injector.get('$window');
-    optionsFactory = $injector.get('optionsFactory');
-    OrdersFactory = $injector.get('OrdersFactory');
-    var $controller = $injector.get('$controller');
-    $scope = $rootScope.$new();
+        $rootScope = $injector.get('$rootScope');
+        $state = $injector.get('$state');
+        $window = $injector.get('$window');
+        optionsFactory = $injector.get('optionsFactory');
+        OrdersFactory = $injector.get('OrdersFactory');
+        var $controller = $injector.get('$controller');
+        $scope = $rootScope.$new();
 
-    createController = function () {
-      return $controller('BarQueueController', {
+        createController = function() {
+            return $controller('BarQueueController', {
 
-        $scope: $scope,
-        $window: $window,
-        $state: $state,
-        optionsFactory: optionsFactory,
-        OrdersFactory: OrdersFactory
-      });
-    };
-    createController();
-  }));
+                $scope: $scope,
+                $window: $window,
+                $state: $state,
+                optionsFactory: optionsFactory,
+                OrdersFactory: OrdersFactory
+            });
+        };
+        createController();
+    }));
 
-  it('should have a bartenderLogout function', function () {
-    expect($scope.bartenderLogout).toEqual(jasmine.any(Function));
-  });
+    it('should have a bartenderLogout function', function() {
+        expect($scope.bartenderLogout).toEqual(jasmine.any(Function));
+    });
 
-  it('should have a getOrders function', function () {
-    expect($scope.getOrders).toEqual(jasmine.any(Function));
-  });
+    it('should have a data object as a property', function() {
+        expect($scope.data).toEqual(jasmine.any(Object));
+    });
 
-  it('should have a dequeue function', function () {
-    expect($scope.dequeue).toEqual(jasmine.any(Function));
-  });
+    it('should have a completeOrder function', function() {
+        expect($scope.completeOrder).toEqual(jasmine.any(Function));
+    });
 
 });
